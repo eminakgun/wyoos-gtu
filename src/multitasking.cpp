1abbebb8cf35dc1c1304e7899a0dcdf64efbe64e
eminakgun <eminakgn@outlook.com>
2024-05-23 23:06:40 +0300

    Add comments

--- src/multitasking.cpp
+++ src/multitasking.cpp
@@ -76,52 +76,50 @@ bool TaskManager::AddTask(Task* task)
     if(numTasks >= 256)
         return false;
    
+    // set pid as zero for the first process ever
+    // since we never want to return pid of zero
     if (numTasks == 0){
-        printf("set parent pid\n");
         task->pcb.pid = 1;
     }
 
     tasks[numTasks++] = task;
-    
-/*     printf("\nTaskManager::AddTask::1, currentTask:");
-    printInt(currentTask);
-    printf("\nTaskManager::AddTask::1, numTasks:");
-    printInt(numTasks);
-    printf("\n"); */
     return true;
 }
 
 Task* TaskManager::AddTask() {
     
+    // Create a new Task object and initialize it with nullptr for initial values
     Task* new_task = new Task(nullptr, nullptr);
     
+    // Add the new task to the task list
     tasks[numTasks] = new_task;
     ++numTasks;
 
+    // Set the PID (Process ID) of the new task to the current number of tasks
+    // so that pids are unique since we don't decrement numTasks ever
     new_task->pcb.pid = numTasks;
+
+    // Set the PPID (Parent Process ID) of the new task to the PID of the current task
     new_task->pcb.ppid = tasks[currentTask]->pcb.pid;
     
-    // copy stack of the current process to new task
+    // Copy stack of the current process to the new task
+    // This is done to create a new task that starts with the same stack state as the current one
     for (size_t i = 0; i < sizeof(tasks[currentTask]->stack); i++){
         new_task->stack[i] = tasks[currentTask]->stack[i];
     }
-
-/*     printf("\nTaskManager::AddTask::2, currentTask:");
-    printInt(currentTask);
-    printf("\nTaskManager::AddTask::2, numTasks:");
-    printInt(numTasks); */
 
     return new_task;
 }
 
 int TaskManager::WaitTask(uint32_t pid){
 
-    if (tasks[currentTask]->pcb.pid == pid) // myself
+    // Check if the current task's PID matches the specified PID (trying to wait on itself)
+    if (tasks[currentTask]->pcb.pid == pid)
     {
         return 0;
     }
     
-    // find target task
+    // Search for the task with the specified PID in the task list
     Task* target = nullptr;
     for (int i = 0; i < numTasks; i++) {
         if (tasks[i]->pcb.pid ==pid) {
@@ -132,35 +130,41 @@ int TaskManager::WaitTask(uint32_t pid){
     if (target != nullptr)
     {
         /* printf("Found a process to wait\n"); */
+        // Update the current task's PCB (Process Control Block) to indicate it is waiting
         tasks[currentTask]->pcb.state = WAITING;
-        tasks[currentTask]->pcb.waitpid = pid;
-        tasks[currentTask]->pcb.waitstate = target->pcb.state;
+        tasks[currentTask]->pcb.waitpid = pid; // Set the PID of the task it is waiting for
+        tasks[currentTask]->pcb.waitstate = target->pcb.state; // Set the wait state to the target task's state
+        
+        // Return 1 indicating the task is now waiting on the specified PID
         return 1;
     }
     else
+        // If no task with the specified PID was found, return 0
         return 0;
 }
 
 CPUState* TaskManager::Schedule(CPUState* cpustate){
     //printTasks();
 
+    // If there are no tasks, return the current CPU state
     if(numTasks <= 0) {
         return cpustate;
     }
     
+    // If no task is currently scheduled, set the current task to the first task (index 0)
     if (currentTask == -1) {
         // always run main process if there's nothing scheduled
         currentTask = 0;
     }
     else if(currentTask >= 0)
-        // save stack pointer
+        // Save the CPU state(stack pointer) of the currently scheduled task
         tasks[currentTask]->cpustate = cpustate;   
     
     // Schedule next available process
     Task* next_task = nullptr;
     int next_task_num = 0;
 
-    // Check if any of them is in waiting state
+    // Check if any task is in the waiting state
     Task* waiting = nullptr;
     int wait_task_num;
     for (int i = 0; i < numTasks; i++) {
@@ -169,6 +173,8 @@ CPUState* TaskManager::Schedule(CPUState* cpustate){
             wait_task_num = i;
         }
     }
+
+    // If there is a task in the waiting state, check if the process it's waiting for has terminated
     if (waiting != nullptr)
     {
         //printf("There's a waiting process\n");
@@ -176,6 +182,7 @@ CPUState* TaskManager::Schedule(CPUState* cpustate){
         for (int i = 0; i < numTasks; i++) {
             if (tasks[i]->pcb.pid == waiting->pcb.waitpid) {
                 if (tasks[i]->pcb.state == TERMINATED){
+                    // If the process being waited for has terminated, update next_task and its index
                     //printf("Being waited process has changed its state!\n");
                     waiting->pcb.waitpid = 0;
                     next_task = waiting;
@@ -187,14 +194,12 @@ CPUState* TaskManager::Schedule(CPUState* cpustate){
         }
     }
 
+    // If next_task is still nullptr, find the next ready process to schedule
     if (next_task == nullptr)
     {
         //printf("Schedule next READY process");
         for (int i = currentTask; i < numTasks; )
         {
-/*             printf("process");
-            printInt(i);
-            printf("\n"); */
             if(++i >= numTasks)
                 i %= numTasks;
             
@@ -223,6 +228,7 @@ CPUState* TaskManager::Schedule(CPUState* cpustate){
         tasks[currentTask]->pcb.state = READY;
     }
 
+    // Set the state of the next task to RUNNING and update currentTask
     next_task->pcb.state = RUNNING;
     currentTask = next_task_num;
 
